9035122a24615e9735fb3c1204f92d6f1c35225e
Noah Hall <noah.hall@dictionary.com>
2017-01-08 12:01:33 -0800

    update dist

--- dist/lib/axes.js
+++ dist/lib/axes.js
@@ -1,169 +1,20 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.getYAxisLabel = exports.getXAxisLabel = exports.getXAxis = exports.getYAxis = undefined;
-
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; // import * as label from './labels.js';
-
-
-var _text = require('../svg/text.js');
-
-var _text2 = _interopRequireDefault(_text);
-
-var _d = require('../../node_modules/d3/build/d3.node');
-
-var d3 = _interopRequireWildcard(_d);
-
-var _react = require('../../node_modules/react/react');
-
-var _react2 = _interopRequireDefault(_react);
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-/*
+'use strict';Object.defineProperty(exports,'__esModule',{value:!0});exports.getYAxisLabel=exports.getXAxisLabel=exports.getXAxis=exports.getYAxis=void 0;var _typeof='function'==typeof Symbol&&'symbol'==typeof Symbol.iterator?function(obj){return typeof obj}:function(obj){return obj&&'function'==typeof Symbol&&obj.constructor===Symbol&&obj!==Symbol.prototype?'symbol':typeof obj};// import * as label from './labels.js';
+var _text=require('../svg/text.js'),_text2=_interopRequireDefault(_text),_d=require('d3'),d3=_interopRequireWildcard(_d),_react=require('react'),_react2=_interopRequireDefault(_react);function _interopRequireWildcard(obj){if(obj&&obj.__esModule)return obj;var newObj={};if(null!=obj)for(var key in obj)Object.prototype.hasOwnProperty.call(obj,key)&&(newObj[key]=obj[key]);return newObj.default=obj,newObj}function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj}}/*
  * Create/Update Y Axis in the DOM
- */
-var getYAxis = exports.getYAxis = function getYAxis(_ref) {
-  var _ref$id = _ref.id,
-      id = _ref$id === undefined ? '' : _ref$id,
-      _ref$thisYScale = _ref.thisYScale,
-      thisYScale = _ref$thisYScale === undefined ? null : _ref$thisYScale;
-
-  if (!id || !thisYScale) {
-    appFuncs.logError({
-      data: [id, thisYScale, typeof document === 'undefined' ? 'undefined' : _typeof(document)],
-      loc: __filename,
-      msg: 'id and thisYScale must be valid variables in axes.getYAxis(), returning null'
-    });
-
-    return null;
-  }
-
-  // dont create axis when rendering on server
-  if (typeof document === 'undefined') return null;
-
-  return d3 // eslintignore let d3 handle the axis instead of building ourselves
-  .select(document.getElementById('' + id)).select('.y.axis').call(d3.axisLeft(thisYScale)).selectAll("text").classed('axies text', true);
-};
-
-/*
+ */var getYAxis=exports.getYAxis=function getYAxis(_ref){var _ref$id=_ref.id,a=void 0===_ref$id?'':_ref$id,_ref$thisYScale=_ref.thisYScale,b=void 0===_ref$thisYScale?null:_ref$thisYScale;return a&&b?'undefined'==typeof document?null:d3// eslintignore let d3 handle the axis instead of building ourselves
+.select(document.getElementById(''+a)).select('.y.axis').call(d3.axisLeft(b)).selectAll('text').classed('axies text',!0):(appFuncs.logError({data:[a,b,'undefined'==typeof document?'undefined':_typeof(document)],loc:__filename,msg:'id and thisYScale must be valid variables in axes.getYAxis(), returning null'}),null);// dont create axis when rendering on server
+};/*
  * Create/Update X Axis and insert it in DOM
- */
-var getXAxis = exports.getXAxis = function getXAxis(_ref2) {
-  var _ref2$dx = _ref2.dx,
-      dx = _ref2$dx === undefined ? '-.8em' : _ref2$dx,
-      _ref2$dy = _ref2.dy,
-      dy = _ref2$dy === undefined ? '.15em' : _ref2$dy,
-      _ref2$id = _ref2.id,
-      id = _ref2$id === undefined ? '' : _ref2$id,
-      _ref2$textAnchor = _ref2.textAnchor,
-      textAnchor = _ref2$textAnchor === undefined ? 'end' : _ref2$textAnchor,
-      _ref2$thisXScale = _ref2.thisXScale,
-      thisXScale = _ref2$thisXScale === undefined ? null : _ref2$thisXScale,
-      _ref2$transform = _ref2.transform,
-      transform = _ref2$transform === undefined ? 'rotate(-65)' : _ref2$transform;
-
-  if (!id || !thisXScale) {
-    appFuncs.logError({
-      data: [id, thisXScale, typeof document === 'undefined' ? 'undefined' : _typeof(document)],
-      loc: __filename,
-      msg: 'id and thisXScale must be valid variables in axes.getXAxis(), returning null'
-    });
-
-    return null;
-  }
-
-  // dont create axis when rendering on server
-  if (typeof document === 'undefined') return null;
-
-  return d3 // eslintignore let d3 handle the axis instead of building ourselves
-  .select(document.getElementById('' + id)).select('.x.axis').call(d3.axisBottom(thisXScale)).selectAll('g.tick text').classed('axes text', true).attr('dx', dx).attr('dy', dy).attr('transform', transform).style('text-anchor', textAnchor);
-};
-
-/**
+ */var getXAxis=exports.getXAxis=function getXAxis(_ref2){var _ref2$dx=_ref2.dx,a=void 0===_ref2$dx?'-.8em':_ref2$dx,_ref2$dy=_ref2.dy,b=void 0===_ref2$dy?'.15em':_ref2$dy,_ref2$id=_ref2.id,c=void 0===_ref2$id?'':_ref2$id,_ref2$textAnchor=_ref2.textAnchor,d=void 0===_ref2$textAnchor?'end':_ref2$textAnchor,_ref2$thisXScale=_ref2.thisXScale,e=void 0===_ref2$thisXScale?null:_ref2$thisXScale,_ref2$transform=_ref2.transform,f=void 0===_ref2$transform?'rotate(-65)':_ref2$transform;return c&&e?'undefined'==typeof document?null:d3// eslintignore let d3 handle the axis instead of building ourselves
+.select(document.getElementById(''+c)).select('.x.axis').call(d3.axisBottom(e)).selectAll('g.tick text').classed('axes text',!0).attr('dx',a).attr('dy',b).attr('transform',f).style('text-anchor',d):(appFuncs.logError({data:[c,e,'undefined'==typeof document?'undefined':_typeof(document)],loc:__filename,msg:'id and thisXScale must be valid variables in axes.getXAxis(), returning null'}),null);// dont create axis when rendering on server
+};/**
  * Positions label on x Axis
  * @method getXAxisLabel
  * @param  {String}      [xAxisLabel=''}] [description]
  * @return {Boolean}     [description]
- */
-var getXAxisLabel = exports.getXAxisLabel = function getXAxisLabel(_ref3) {
-  var _ref3$transform = _ref3.transform,
-      transform = _ref3$transform === undefined ? 'rotate(0)' : _ref3$transform,
-      _ref3$x = _ref3.x,
-      x = _ref3$x === undefined ? 5 : _ref3$x,
-      _ref3$xAxisLabel = _ref3.xAxisLabel,
-      xAxisLabel = _ref3$xAxisLabel === undefined ? '' : _ref3$xAxisLabel,
-      _ref3$y = _ref3.y,
-      y = _ref3$y === undefined ? -5 : _ref3$y;
-
-  if (!xAxisLabel) {
-    appFuncs.logError({
-      data: xAxisLabel,
-      loc: __filename,
-      msg: 'id and thisYScale must be valid variables in axes.getYAxis(), returning null'
-    });
-
-    return null;
-  }
-
-  return _react2.default.createElement(_text2.default, {
-    chartType: 'axes',
-    text: xAxisLabel,
-    transform: transform,
-    x: x,
-    y: y
-  });
-};
-
-getXAxisLabel.propTypes = {
-  transform: _react2.default.PropTypes.string,
-  x: _react2.default.PropTypes.number,
-  xAxisLabel: _react2.default.PropTypes.string,
-  y: _react2.default.PropTypes.number
-};
-
-/**
+ */var getXAxisLabel=exports.getXAxisLabel=function getXAxisLabel(_ref3){var _ref3$transform=_ref3.transform,a=void 0===_ref3$transform?'rotate(0)':_ref3$transform,_ref3$x=_ref3.x,b=void 0===_ref3$x?5:_ref3$x,_ref3$xAxisLabel=_ref3.xAxisLabel,c=void 0===_ref3$xAxisLabel?'':_ref3$xAxisLabel,_ref3$y=_ref3.y,d=void 0===_ref3$y?-5:_ref3$y;return c?_react2.default.createElement(_text2.default,{chartType:'axes',text:c,transform:a,x:b,y:d}):(appFuncs.logError({data:c,loc:__filename,msg:'id and thisYScale must be valid variables in axes.getYAxis(), returning null'}),null)};getXAxisLabel.propTypes={transform:_react2.default.PropTypes.string,x:_react2.default.PropTypes.number,xAxisLabel:_react2.default.PropTypes.string,y:_react2.default.PropTypes.number};/**
  * Positions label on y Axis
  * @method getXAxisLabel
  * @param  {String}      [xAxisLabel=''}] [description]
  * @return {Boolean}     [description]
- */
-var getYAxisLabel = exports.getYAxisLabel = function getYAxisLabel(_ref4) {
-  var _ref4$transform = _ref4.transform,
-      transform = _ref4$transform === undefined ? 'rotate(-90)' : _ref4$transform,
-      _ref4$x = _ref4.x,
-      x = _ref4$x === undefined ? 5 : _ref4$x,
-      _ref4$y = _ref4.y,
-      y = _ref4$y === undefined ? -5 : _ref4$y,
-      _ref4$yAxisLabel = _ref4.yAxisLabel,
-      yAxisLabel = _ref4$yAxisLabel === undefined ? '' : _ref4$yAxisLabel;
-
-  if (!yAxisLabel) {
-    appFuncs.logError({
-      data: [transform, x, y, yAxisLabel],
-      loc: __filename,
-      msg: 'yAxisLabel must be a valid variable in axes.getYAxisLabel(), returning empty string'
-    });
-
-    return '';
-  }
-
-  return _react2.default.createElement(_text2.default, {
-    chartType: 'axes',
-    text: yAxisLabel,
-    transform: transform,
-    x: x,
-    y: y
-  });
-};
-
-getYAxisLabel.propTypes = {
-  transform: _react2.default.PropTypes.string,
-  x: _react2.default.PropTypes.oneOfType([_react2.default.PropTypes.number, _react2.default.PropTypes.string]),
-  y: _react2.default.PropTypes.oneOfType([_react2.default.PropTypes.number, _react2.default.PropTypes.string]),
-  yAxisLabel: _react2.default.PropTypes.string
-};
+ */var getYAxisLabel=exports.getYAxisLabel=function getYAxisLabel(_ref4){var _ref4$transform=_ref4.transform,a=void 0===_ref4$transform?'rotate(-90)':_ref4$transform,_ref4$x=_ref4.x,b=void 0===_ref4$x?5:_ref4$x,_ref4$y=_ref4.y,c=void 0===_ref4$y?-5:_ref4$y,_ref4$yAxisLabel=_ref4.yAxisLabel,d=void 0===_ref4$yAxisLabel?'':_ref4$yAxisLabel;return d?_react2.default.createElement(_text2.default,{chartType:'axes',text:d,transform:a,x:b,y:c}):(appFuncs.logError({data:[a,b,c,d],loc:__filename,msg:'yAxisLabel must be a valid variable in axes.getYAxisLabel(), returning empty string'}),'')};getYAxisLabel.propTypes={transform:_react2.default.PropTypes.string,x:_react2.default.PropTypes.oneOfType([_react2.default.PropTypes.number,_react2.default.PropTypes.string]),y:_react2.default.PropTypes.oneOfType([_react2.default.PropTypes.number,_react2.default.PropTypes.string]),yAxisLabel:_react2.default.PropTypes.string};
--- dist/lib/tooltip.js
+++ dist/lib/tooltip.js
@@ -1,70 +1,8 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.ToolTip = undefined;
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _react = require('../../node_modules/react/react');
-
-var _react2 = _interopRequireDefault(_react);
-
-var _d = require('../../node_modules/d3/build/d3.node');
-
-var d3 = _interopRequireWildcard(_d);
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var ToolTip = exports.ToolTip = function (_React$Component) {
-  _inherits(ToolTip, _React$Component);
-
-  function ToolTip(props) {
-    _classCallCheck(this, ToolTip);
-
-    return _possibleConstructorReturn(this, (ToolTip.__proto__ || Object.getPrototypeOf(ToolTip)).call(this, props));
-  }
-
-  _createClass(ToolTip, [{
-    key: 'componentDidMount',
-    value: function componentDidMount() {
-      // single tooltip used for all charts
-      var appToolTip = d3.select('body').append('section').style('background', 'black').style('border', '2px red dashed').style('borderRadius', '4px').style('opacity', 0).style('padding', '10px').style('position', 'absolute');
-
-      // this needs to be inside somed3Component.on() mouseover & click
-      var appToolTipTransition = d3.transition().duration(250).delay(100).ease(d3.easePolyIn);
-
-      try {
-        appToolTip.transition(appToolTipTransition).style('opacity', 1).style('color', 'white').style('left', d3.event.pageX + 'px').style('top', d3.event.pageY + 'px');
-      } catch (err) {
-        // do nothing on err when too many interrupts of transitions
-      }
-
-      appToolTip.html(d);
-
-      // this needs to be inside somed3Component.on() mouseout
-      try {
-        // eslintignore if too many transitions, will throw err, read https://github.com/d3/d3-transition#the-life-of-a-transition
-        appToolTip.transition(appToolTipTransition).style('opacity', 0);
-      } catch (err) {
-        appFuncs.console('dir')(err);
-      }
-    }
-  }]);
-
-  return ToolTip;
-}(_react2.default.Component);
-
-/*
+'use strict';Object.defineProperty(exports,'__esModule',{value:!0});exports.ToolTip=void 0;var _createClass=function(){function defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||!1,descriptor.configurable=!0,'value'in descriptor&&(descriptor.writable=!0),Object.defineProperty(target,descriptor.key,descriptor)}}return function(Constructor,protoProps,staticProps){return protoProps&&defineProperties(Constructor.prototype,protoProps),staticProps&&defineProperties(Constructor,staticProps),Constructor}}();var _react=require('react'),_react2=_interopRequireDefault(_react),_d=require('d3'),d3=_interopRequireWildcard(_d);function _interopRequireWildcard(obj){if(obj&&obj.__esModule)return obj;var newObj={};if(null!=obj)for(var key in obj)Object.prototype.hasOwnProperty.call(obj,key)&&(newObj[key]=obj[key]);return newObj.default=obj,newObj}function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj}}function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor))throw new TypeError('Cannot call a class as a function')}function _possibleConstructorReturn(self,call){if(!self)throw new ReferenceError('this hasn\'t been initialised - super() hasn\'t been called');return call&&('object'==typeof call||'function'==typeof call)?call:self}function _inherits(subClass,superClass){if('function'!=typeof superClass&&null!==superClass)throw new TypeError('Super expression must either be null or a function, not '+typeof superClass);subClass.prototype=Object.create(superClass&&superClass.prototype,{constructor:{value:subClass,enumerable:!1,writable:!0,configurable:!0}}),superClass&&(Object.setPrototypeOf?Object.setPrototypeOf(subClass,superClass):subClass.__proto__=superClass)}var ToolTip=exports.ToolTip=function(_React$Component){function ToolTip(a){return _classCallCheck(this,ToolTip),_possibleConstructorReturn(this,(ToolTip.__proto__||Object.getPrototypeOf(ToolTip)).call(this,a))}return _inherits(ToolTip,_React$Component),_createClass(ToolTip,[{key:'componentDidMount',value:function componentDidMount(){// single tooltip used for all charts
+var a=d3.select('body').append('section').style('background','black').style('border','2px red dashed').style('borderRadius','4px').style('opacity',0).style('padding','10px').style('position','absolute'),b=d3.transition().duration(250).delay(100).ease(d3.easePolyIn);// this needs to be inside somed3Component.on() mouseover & click
+try{a.transition(b).style('opacity',1).style('color','white').style('left',d3.event.pageX+'px').style('top',d3.event.pageY+'px')}catch(c){// do nothing on err when too many interrupts of transitions
+}a.html(d);// this needs to be inside somed3Component.on() mouseout
+try{a.transition(b).style('opacity',0)}catch(c){appFuncs.console('dir')(c)}}}]),ToolTip}(_react2.default.Component);/*
 // single tooltip used for all charts
 const appToolTip = d3.select(`#${this.props.id}-tooltip`);
 

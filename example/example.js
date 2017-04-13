5d60e1f7678bf326d102f5d6a1189308828e8c42
Noah Edward Hall <noahedwardhall@gmail.com>
2017-04-13 14:45:27 -0700

    add raw twitter data

--- example/example.js
+++ example/example.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Chart from '../dist/index.js';
-import data from './fakedata/packtwitter.json';
+//import packData from './fakedata/packtwitter.json';
+import timelineData from './fakedata/rawtwitter.json';
 
 export default class Table extends React.Component {
   static get defaultProps () {
     return {
-      id: 'fake-table',
+      id: 'fake-chart',
     };
   }
 
+  /* pack
   render () {
     return (
       <section style={{ maxWidth: '1000px' }}>
@@ -16,7 +18,7 @@ export default class Table extends React.Component {
           chartType='pack'
           colorScaleScheme='schemeCategory20'
           colorScaleType='basic'
-          data={data}
+          data={packData}
           datumLabels={['name']}
           figureObject={true}
           figureObjectType='table'
@@ -31,4 +33,42 @@ export default class Table extends React.Component {
       </section>
     );
   }
+  //*/
+
+  //* timeline
+   // y = sum aggregate sentiment for day
+   // x = dates
+   // groupBy = username ? possibly best option
+    // could do: token
+  render () {
+    return (
+      <Chart
+        chartDataGroupBy='type'
+        chartType='line'
+        colorScaleScheme='schemeCategory10'
+        colorScaleType='basic'
+        data={timelineData}
+        datumLabels={['total']}
+        id='fake-chart'
+        margins={{
+          bottom: 70,
+          left: 70,
+          right: 10,
+          top: 10,
+        }}
+        xAxis={true}
+        xAxisLabel='Date'
+        xScale={true}
+        xScaleTime={true}
+        xScaleTimeFormat='%Y/%m/%d'
+        xValue='date'
+        yAxis={true}
+        yAxisLabel='Total Paying Customers'
+        yScale={true}
+        yValue='totalPayingCustomers'
+      />
+    );
+  }
+  //*/
+
 }
